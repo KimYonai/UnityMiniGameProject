977fcaf515b7cbc8a6aa87d7f6966709e2d81cf9
KimYonai <msb0714@gmail.com>
2024-09-29 14:19:10 +0900

    [Add] GameManager Singleton
    게임의 전체적인 관리를 위한 관리자를 싱글톤으로 추가했습니다.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance = null;
+
+    private void Awake()
+    {
+        if (null == instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+
+            return instance;
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
